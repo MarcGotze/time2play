119a04be6cce8b82d72950bac50a51eedef8478e
Eleonore Baillot <eleonorebaillot@gmail.com>
2023-08-29 12:16:11 +0200

    generate actions new create edit update destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :boardgames, only: [:index, :new, :create, :show]
   # Defines the root path route ("/")
   # root "articles#index"
-
+  resources :boardgames, only: [:index, :new, :create, :show]
+  resources :users, only: %i[new create edit update destroy]
   resources :parties, only: %i[show edit update new create destroy]
 
 end
