40d2a0621e19e3081ca3914b211abaf283349289
Marc Gotze marc.gotze@gmail.com <marc.gotze@gmail.com>
2023-08-29 18:08:31 +0200

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "boardgames#index"
   resources :boardgames, only: %i[index new create show]
   resources :players, only: %i[new create destroy]
   resources :users, only: %i[new create edit update destroy]
