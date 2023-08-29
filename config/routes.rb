2ae0e01a99036de6c25b7052b4d905597d60fbc4
sarah tabet <sarah.tabet69@gmail.com>
2023-08-29 12:18:24 +0200

    controller players

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :boardgames, only: [:index, :new, :create, :show]
+  resources :boardgames, only: %i[index new create show]
+  resources :players, only: %i[new create destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 
   resources :parties, only: %i[show edit update new create destroy]
-
 end
