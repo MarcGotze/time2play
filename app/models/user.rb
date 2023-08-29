3a4a5b5a7e6d990362732234248ab39b49f0bad4
Eleonore Baillot <eleonorebaillot@gmail.com>
2023-08-29 11:49:21 +0200

    devise sign up sign in init

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :parties
+
+  validates :username, presence: true
 end
