1b028a0e8554ea176b44ce7cad041eeb990d866a
Marc Gotze marc.gotze@gmail.com <marc.gotze@gmail.com>
2023-08-30 17:11:40 +0200

    Profil route set

--- app/models/boardgame.rb
+++ app/models/boardgame.rb
@@ -3,9 +3,7 @@ class Boardgame < ApplicationRecord
   has_many :game_to_users, dependent: :destroy
   has_many :players, through: :parties
 
-  validates :title, :description, :play_time, :age, :image,
+  validates :title, :description, :play_time, :age,
   :rating, :min_player, :max_player, :category, presence: true
   has_one_attached :photo
-
-
 end
