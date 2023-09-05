da8d7c35c66df1779784cb7ec6b2d491934ca9ea
Marc Gotze marc.gotze@gmail.com <marc.gotze@gmail.com>
2023-09-05 17:47:05 +0200

    profil style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,8 @@ class UsersController < ApplicationController
     @challenges = @user.challenges
     @achievements = @user.achievements
     @victories = @parties.select { |party| party.players.order(:score).last.user == current_user }.count
+    @total_min = 0
+    @total_hours = 0
+    @total_days = 0
   end
 end
