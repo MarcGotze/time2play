526cb25e67695ff01a3146fbc7a0be53cfaf55d0
Eleonore Baillot <eleonorebaillot@gmail.com>
2023-09-06 09:43:44 +0200

    added victory based challenges with reward

--- app/models/party.rb
+++ app/models/party.rb
@@ -3,13 +3,12 @@ class Party < ApplicationRecord
   has_many :players, dependent: :destroy
   has_many :users, through: :players
   validates :start_time, presence: true
-  after_update :challenge_calcul, if: -> { end_time.present? }
+  after_update :challenge_calcul_scores, if: -> { end_time.present? }
+  after_update :challenge_calcul_victories, if: -> { end_time.present? }
 
   private
 
-  def challenge_calcul
-    # ENTREE
-    # SORTIE : update achivement.completed = true
+  def challenge_calcul_scores
     players.each do |player|
       boardgame.challenges.each do |challenge|
         next unless player.score >= challenge.score
@@ -20,4 +19,17 @@ class Party < ApplicationRecord
       end
     end
   end
+
+  def challenge_calcul_victories
+    players.each do |player|
+      boardgame.challenges.each do |challenge|
+        next unless player.user.victories == challenge.victory
+        challenge.achievements.where(user: player.user).each do |achievement|
+          achievement.update(completed: true)
+          achievement.user.update(level: achievement.user.level += 1)
+        end
+      end
+    end
+  end
+  
 end
